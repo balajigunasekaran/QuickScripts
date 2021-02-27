24f59fb1f76fe42886cd3904f26ec4b8c5ff449c
Balaji Gunasekaran <balaji.gunasekaran@live.com>
2021-02-27 11:01:18 +0100

    Update ProjectReferences.ps1

--- ProjectReferences.ps1
+++ ProjectReferences.ps1
@@ -1,19 +1,23 @@
-Function Get-ProjectReferences ($rootFolder)
-{
-    $projectFiles = Get-ChildItem $rootFolder -Filter *.csproj -Recurse
+param(
+    [Parameter(Mandatory = $true)][string]$path
+)
+
+$projectFiles = Get-ChildItem $path -Filter *.csproj -Recurse
     
-    $projectFiles | ForEach-Object {
-        $projectFile = $_ | Select-Object -ExpandProperty FullName
-        $projectName = $_ | Select-Object -ExpandProperty BaseName
-        $projectXml = [xml](Get-Content $projectFile)
+$projectFiles | ForEach-Object {
+    $projectFile = $_ | Select-Object -ExpandProperty FullName
+    $projectName = $_ | Select-Object -ExpandProperty BaseName
+    $projectXml = [xml](Get-Content $projectFile)
 
-        $projectReferences = $projectXml | Select-Xml '//ProjectReference' | Select-Object -ExpandProperty Node | Select-Object Include
+    $projectReferences = $projectXml | Select-Xml '//ProjectReference' | Select-Object -ExpandProperty Node
 
+    if ($projectReferences.Count -gt 0) {
         $projectReferences | ForEach-Object {
-            "[" + $projectName + "] -> [" + $_ + "]"
-        }
-        "=============================================================================================="
+            "[" + $projectName + "] -> [" + $_.Include + "]"
+        }    
     }
+    else {
+        "[" + $projectName + "]"
+    }
+    "=============================================================================================="
 }
-
-Get-ProjectReferences "D:\Source\Git\proarcapi\Source" | Out-File "D:\Source\Git\References.txt"
